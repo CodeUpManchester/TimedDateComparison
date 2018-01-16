fb8eb019713d10b6abdfe397401cb1ea85aec5ff
FERMI\Drew <dangerworm@gmail.com>
2018-01-16 22:19:43 +0000

    Initial commit for Stuart to have a look at.

---
+++ TimedDateComparison/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace TimedDateComparison
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private const int CheckPeriodInSeconds = 4;
+
+        public SolidColorBrush DateColour { get; private set; }
+        public DateTime RegisteredSubkey { get; private set; }
+        public DateTime CurrentDate => DateTime.Now;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void NotifyPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeWindow();
+
+            var dateInterval = new TimeSpan(0, 0, CheckPeriodInSeconds / 2);
+            new DispatcherTimer(dateInterval, DispatcherPriority.Background, CheckDate, Dispatcher.CurrentDispatcher);
+
+            var colorInterval = new TimeSpan(0, 0, CheckPeriodInSeconds);
+            new DispatcherTimer(colorInterval, DispatcherPriority.Background, CheckColour, Dispatcher.CurrentDispatcher);
+
+            CheckColour(this, EventArgs.Empty);
+        }
+
+        private void InitializeWindow()
+        {
+            RegisteredSubkey = DateTime.Today.AddDays(-1);
+            NotifyPropertyChanged(nameof(RegisteredSubkey));
+        }
+
+        private void CheckDate(object sender, EventArgs e)
+        {
+            if (RegisteredSubkey.Equals(DateTime.Today))
+            {
+                return;
+            }
+
+            RegisteredSubkey = DateTime.Today;
+            NotifyPropertyChanged(nameof(RegisteredSubkey));
+        }
+
+        private void CheckColour(object sender, EventArgs e)
+        {
+            DateColour = RegisteredSubkey < DateTime.Today
+                ? Brushes.Red
+                : Brushes.LimeGreen;
+            NotifyPropertyChanged(nameof(DateColour));
+        }
+    }
+}
